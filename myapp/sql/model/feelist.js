e82b4578a78eceb1a083a7321c7c594cd1552611
TuGe7025 <1109320711@qq.com>
2019-07-14 17:01:05 +0800

    去除myapp本地服务器

--- myapp/sql/model/feelist.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('../db');
-const { Schema } = mongoose;
-const feelistSchema = new Schema({
-  data: {type: Object}
-})
-module.exports = mongoose.model('Feelist', feelistSchema, 'feedlist');
