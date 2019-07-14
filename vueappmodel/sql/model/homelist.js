d8b61bb98652727a27d1b0fba21058a3497884e8
TuGe7025 <1109320711@qq.com>
2019-07-14 17:12:31 +0800

    恢复src文件数据

--- vueappmodel/sql/model/homelist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../db.js')
-const Schema = mongoose.Schema
-const HomelistSchema = new Schema({
-  code: {type: Int32Array },
-  message: {type: null},
-  data: {type: Object},
-  errtag: {type: Int32Array}
-})
-module.exports = mongoose.model('Homelist', HomelistSchema)
---
+++ vueappmodel/src/views/Order/Order.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="order">
+    <header class="order-header">我的订单</header>
+    <nav class="order-nav">订单导航</nav>
+    <div class="order-list">订单列表</div>
+  </div>
+</template>
