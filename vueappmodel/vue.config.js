d9238423013cfac4e9bc13812a94b29e543a8a17
TuGe7025 <1109320711@qq.com>
2019-07-15 22:36:31 +0800

    合并了新的

--- vueappmodel/vue.config.js
+++ vueappmodel/vue.config.js
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
 // vue.config.js
-=======
->>>>>>> 72a58b6dc2c435d56d9f2864e6c6fc76182a6d03
 module.exports = {
   devServer: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
-        target: 'http://10.11.56.83:3000', //对应自己的接口
+        target: 'http://192.168.1.100:3000', //对应自己的接口
         changeOrigin: true,
         ws: true,
-=======
-        target: 'http://10.11.56.83:3000',
-        changeOrigin: true,
->>>>>>> 72a58b6dc2c435d56d9f2864e6c6fc76182a6d03
         pathRewrite: {
           '^/api': ''
         }
       }
     }
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 72a58b6dc2c435d56d9f2864e6c6fc76182a6d03
