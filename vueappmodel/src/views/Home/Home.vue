c4bda024f7ec87328ecda5d826be79a2dbccccf4
TuGe7025 <1109320711@qq.com>
2019-07-15 17:49:48 +0800

    home基基本布局已完成

--- vueappmodel/src/views/Home/Home.vue
+++ vueappmodel/src/views/Home/Home.vue
@@ -33,44 +33,28 @@
         </div> -->
       </nav>
       <div class="home-content">
-        <div class="home-content-goods" :v-if="list.templateId === '4'" v-for="(list, index) of feeds" :key="index">
-          <div class="goods-img">
-            <div class="goods-img-box">
-              <img :src="list.imageUrls"/>
-            </div>
-          </div>
-          <div class="home-content-info">
-            <div class="info-desc">
-              <p class="desc">{{list.title}}</p>
-              <p class="brief">{{list.brief}}</p>
-            </div>
-            <p class="price">
-              <span class="symbol">￥</span>
-              <span class="number" v-for="(pre, idx) of list.priceDesc" :key="idx">{{pre}}</span>
-            </p>
-            <p class="like">{{list.like}}人想要</p>
-          </div>
-        </div>
+        <Goods :feeds="feeds" />
       </div>
     </div>
   </div>
 </template>
 <script>
+import Goods from '@/components/home-goods/goods.vue'
 export default {
   data () {
     return {
       nav: '', // 头部导航数据
-      feeds: '' // 列表数据
+      feeds: [] // 列表数据
     }
+  },
+  components: {
+    Goods
   },
   mounted () {
     fetch('/api/data/datalist')
       .then(res => res.json()).then(data => {
         this.nav = data[0].data.vo.tabs
         this.feeds = data[0].data.vo.feeds.list
-        console.log(data[0].data.vo)
-        console.log(data[0].data.vo.feeds.list)
-        console.log(this.nav)
       })
   }
 }
@@ -84,27 +68,29 @@ export default {
     z-index: 1000;
     @include background-color(#fb7299);
     .home-header-cont {
-      height: 44px;
-      font: 16px/44px "";
+      height: .44rem;
+      font: .16rem/.44rem "";
       text-align: center;
       color: #fff;
     }
   }
   .wrapper {
+    padding-bottom: .3rem;
     position: relative;
     @include rect(100%, 100%);
-    padding-top: 44px;
+    padding-top: .44rem;
     @include overflow(auto);
     .home-nav {
       @include flexbox();
       box-sizing: border-box;
-      @include margin(15px auto 20px);
-      @include padding(0 20px);
+      @include margin(.15rem auto .2rem);
+      @include padding(0 .2rem);
       text-align: center;
       .home-nav-box {
         @include flex();
-        height: 80px;
+        height: .8rem;
         .home-nav-img {
+          overflow:hidden;
           @include rect(.72rem, .72rem);
           @include margin(0 auto);
           img {
@@ -114,8 +100,8 @@ export default {
           }
         }
         p {
-          margin-top: -6px;
-          font-size: 12px;
+          margin-top: -.06rem;
+          font-size: .12rem;
         }
       }
     }
@@ -125,18 +111,18 @@ export default {
       .home-content-goods {
         @include flexbox();
         @include rect(100%, 1.52rem);
-        margin-bottom: 12px;
-        border-radius: 8px;
+        margin-bottom: .12rem;
+        border-radius: .08rem;
         @include background-color(#fff);
-        box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+        box-shadow: 0 .01rem .02rem 0 rgba(0,0,0,.1);
         .goods-img {
           @include rect(1.42rem, 1.42rem);
-          @include margin(5px 0 0 5px);
+          @include margin(.05rem 0 0 .05rem);
           padding-top: 0;
-          border-radius: 4px;
+          border-radius: .04rem;
           .goods-img-box {
             @include overflow(hidden);
-            @include rect(100%, 142px);
+            @include rect(100%, 1.42rem);
             @include background-color(rgb(255, 255, 255));
             img {
               @include display(block);
@@ -145,46 +131,122 @@ export default {
           }
         }
       }
+      .home-content-expo {
+        position: relative;
+        box-sizing: border-box;
+        width: 100%;
+        margin-bottom: .1rem;
+        @include padding(0 .1rem);
+        border-radius: .08rem;
+        background-color: #fff;
+        box-shadow: 0 .01rem .02rem 0 rgba(0,0,0,.1);
+        .expo-head {
+          @include rect(100%, .32rem);
+          .expo-head-title {
+            padding-left: .02rem;
+            font: .12rem/.32rem "";
+            color: #757575;
+          }
+          i {
+            display: inline-block;
+            vertical-align: middle;
+            @include rect(.18rem, .18rem);
+            background-size: 100%;
+          }
+        }
+        .expo-main {
+          @include padding(0 0 .15rem);
+          .expo-main-img {
+            position: relative;
+            width: 100%;
+            padding-top: 62%;
+            margin-bottom: .12rem;
+            border-radius: .04rem;
+            overflow: hidden;
+            .box {
+              @include rect(100%, 1.71rem);
+              @include background-color(rgb(255, 255, 255));
+              position: absolute;
+              top: 0;
+              left: 0;
+              img {
+                @include display(block);
+                @include rect(100%, 100%);
+              }
+            }
+          }
+          .expo-main-name {
+            margin-bottom: .05rem;
+            font: .16rem/.24rem "";
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+          .expo-main-info {
+            margin-bottom: .1rem;
+            font: .12rem/.17rem "";
+            color: #999;
+            @include ellipsis(100%, 2);
+          }
+          .expo-main-price {
+            height: .18rem;
+            color: #ff5687;
+            font: 0px/.18rem;
+            letter-spacing: 0;
+            .symbol {
+              font-size: .14rem;
+            }
+            .number {
+              font-size: .18rem;
+            }
+            .qi {
+              margin-left: .1rem;
+              font-size: .10rem;
+              color: #999;
+            }
+          }
+        }
+      }
       .home-content-info {
         box-sizing: border-box;
         @include flex(1);
         height: 100%;
-        padding-top: 13px;
+        padding-top: .13rem;
         position: relative;
         .info-desc {
-          @include margin(0 11px 0 8px);
+          @include margin(0 .11rem 0 .08rem);
           .desc {
-            margin-bottom: 10px;
-            font: 16px/22px "";
+            margin-bottom: .1rem;
+            font: .16rem/.22rem "";
             @include ellipsis(100%, 2);
           }
           .brief {
             @include ellipsis();
-            height: 12px;
-            font: 12px/12px "";
+            height: .12rem;
+            font: .12rem/.12rem "";
           }
         }
         .price {
           position: absolute;
-          bottom: 12px;
-          left: 8px;
+          bottom: .12rem;
+          left: .08rem;
           height: 18px;
-          font: 0px/12px "";
+          font: 0px/.12rem "";
           letter-spacing: 0;
           color:#ff5687;
           .symbol {
-            font-size: 14px;
+            font-size: .14rem;
           }
           .number {
-            font-size: 18px;
+            font-size: .18rem;
           }
         }
         .like {
           position: absolute;
-          bottom: 12px;
-          right: 10px;
-          height: 12px;
-          font: 12px/12px "";
+          bottom: .12rem;
+          right: .1rem;
+          height: .12rem;
+          font: .12rem/.12rem "";
           color:#ff5687;
         }
       }
